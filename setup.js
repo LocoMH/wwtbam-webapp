2bed9397c5d6de0d5aac4d423ee4147abd32d6f3
LocoMH <till.rasche@gmail.com>
2025-06-04 00:46:45 +0200

    chore: minor code changes

--- setup.js
+++ setup.js
@@ -1,23 +1,7 @@
 $(document).ready(function () {
     // Generate UI elements
     for (let i = 1; i <= 15; i++) {
-        $("#money-tree-area").append(`<span class="money-tree-text money-tree-number version-dependent-formatting" id="money-tree-number-${i}">${i}</span>`)
-        $("#money-tree-area").append(`<span class="money-tree-text money-tree-value version-dependent-formatting" id="money-tree-value-${i}">${i}</span>`)
+        $("#money-tree-overlay").append(`<div class="money-tree-text money-tree-number" id="money-tree-number-${i}">${i}</div>`)
+        $("#money-tree-overlay").append(`<div class="money-tree-text money-tree-value" id="money-tree-value-${i}"></div>`)
     }
-
-    $("#money-tree-value-1").text("€ 50")
-    $("#money-tree-value-2").text("€ 100")
-    $("#money-tree-value-3").text("€ 200")
-    $("#money-tree-value-4").text("€ 300")
-    $("#money-tree-value-5").text("€ 500")
-    $("#money-tree-value-6").text("€ 1.000")
-    $("#money-tree-value-7").text("€ 2.000")
-    $("#money-tree-value-8").text("€ 4.000")
-    $("#money-tree-value-9").text("€ 8.000")
-    $("#money-tree-value-10").text("€ 16.000")
-    $("#money-tree-value-11").text("€ 32.000")
-    $("#money-tree-value-12").text("€ 64.000")
-    $("#money-tree-value-13").text("€ 125.000")
-    $("#money-tree-value-14").text("€ 500.000")
-    $("#money-tree-value-15").text("€ 1 MILLION")
 })
